97b3518406c7f32d3d8d6be9dd00a01d319c505d
BluePandaProjects <willdridge@hotmail.com>
2021-10-30 23:12:13 +0100

    Fixed collider sizes for all gameObjects

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float ballSpeed = 50.0f;
+
+    private Rigidbody2D rigidBodyBall;
+
+    public bool isLaunched = false;
+
+    private void Awake()
     {
-        
+        rigidBodyBall = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey("space") && !isLaunched) 
+        {
+            LaunchBall();
+        }
+    }
+
+    private void LaunchBall()
+    {
+        isLaunched = true;
+
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+
+        rigidBodyBall.velocity = new Vector2(x, y).normalized * ballSpeed;
     }
 }
