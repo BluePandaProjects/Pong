2c793ecd159031427bc119354f8da78c2f2b6675
BluePandaProjects <willdridge@hotmail.com>
2021-11-01 15:59:05 +0000

    This has gone wrong - ignore this branch

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,20 +7,44 @@ public class Ball : MonoBehaviour
     [SerializeField] float ballSpeed = 50.0f;
 
     private Rigidbody2D rigidBodyBall;
+    private bool playGame = false;
 
     public bool isLaunched = false;
+
 
     private void Awake()
     {
         rigidBodyBall = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        StartGame();
+    }
+
     private void Update()
     {
-        if (Input.GetKey("space") && !isLaunched) 
+        if (Input.GetKey("space"))
         {
+            if (!isLaunched)
+            {
+                StartGame();
+            }
+        }
+        else if (playGame)
+        {
+            Invoke("LaunchBall", 1.5f);
+        }
+    }
+
+    private void StartGame()
+    {
+        if (!isLaunched)
+        {
+            playGame = true;
             LaunchBall();
         }
+       
     }
 
     private void LaunchBall()
@@ -36,6 +60,16 @@ public class Ball : MonoBehaviour
     public void ResetBall()
     {
         rigidBodyBall.velocity = Vector2.zero;
-        rigidBodyBall.position = new Vector2(980, 480);
+        rigidBodyBall.position = new Vector2(960, 540);  //This is very hacky
+
+        isLaunched = false;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("GOAL!");
+
+        ResetBall();
+    }
+
 }
